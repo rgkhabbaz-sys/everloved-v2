37be7103b40bba1e70c793d15a668499716e3918
Raji Khabbaz <rgkhabbaz@gmail.com>
2025-12-07 12:03:57 -0500

    UI: Add Global Navigation to Layout (Missing Import)

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,6 +20,7 @@ export default function RootLayout({
         <html lang="en">
             <body className={inter.className}>
                 <ToastProvider>
+                    <Navigation />
                     {children}
                 </ToastProvider>
             </body>
